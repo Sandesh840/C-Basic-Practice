307177ecc1178bfdbd8c22b9067438ce04789438
sandesh840 <gsandesh.840@gmail.com>
2023-03-28 11:20:16 +0545

    first commit

---
+++ first_consol/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.WriteLine("Enter the name :");
+string a = Console.ReadLine();
+Console.WriteLine("Enter the model num :");
+string b = Console.ReadLine();
+Car car = new Car();
+car.name = a;
+car.model = b;
+Console.WriteLine("the car is "+ car.name + " and model number is " + car.model);
+
+class Car
+{
+    public string name { get; set; }
+    public string model { get; set; }
+}
