97d1f5e07b712e1fc298239c4a8656137e3d553d
sandesh840 <gsandesh.840@gmail.com>
2023-04-03 21:44:10 +0545

    condition

--- first_consol/Program.cs
+++ first_consol/Program.cs
@@ -5,6 +5,7 @@ using first_consol.inherite;
 using first_consol.Overloading;
 using first_consol.OverRiding;
 using first_consol.Shape;
+using first_consol.statement;
 using System;
 using System.Runtime.InteropServices;
 // multiple hirearchy
@@ -39,133 +40,201 @@ Bank bank=new Bank();
 bank.setValue(1234);
 Console.WriteLine("the atm pin is " + bank.getReturn());
 */
-Area area = new Area();
-//method overloading
-/*
-area.TSA(5,3);
-Console.WriteLine(area);
-Console.ReadLine();
-*/
 
-/*
-Console.WriteLine("ENter two integer");
-int x=Convert.ToInt32(Console.ReadLine());
-int y=Convert.ToInt32(Console.ReadLine());
-int z = area.Math(x, y);
-Console.WriteLine("The sum is :" + z);
+        //Area area = new Area();
+        //method overloading
+        /*
+        area.TSA(5,3);
+        Console.WriteLine(area);
+        Console.ReadLine();
+        */
 
-Console.WriteLine("ENter two double");
-double l = Convert.ToDouble(Console.ReadLine());
-double m = Convert.ToDouble(Console.ReadLine());
-double n = area.Math(l, m);
-Console.WriteLine("The diference is :" + n);
-*/
+        /*
+        Console.WriteLine("ENter two integer");
+        int x=Convert.ToInt32(Console.ReadLine());
+        int y=Convert.ToInt32(Console.ReadLine());
+        int z = area.Math(x, y);
+        Console.WriteLine("The sum is :" + z);
 
-// Area of rectangle and circle according to input by users
-/*
-Rectangle rectangle=new Rectangle();
-Circle circle=new Circle();
-Console.WriteLine("Type CIRCLE to calculate area of circle, and RECTANGLE to calculate area of rectangle");
-string shape=Console.ReadLine();
+        Console.WriteLine("ENter two double");
+        double l = Convert.ToDouble(Console.ReadLine());
+        double m = Convert.ToDouble(Console.ReadLine());
+        double n = area.Math(l, m);
+        Console.WriteLine("The diference is :" + n);
+        */
 
-
-if (shape == "CIRCLE")
-{
-    circle.Area();
-
-}
-else if (shape=="RECTANGLE")
-{
-    rectangle.Area();
-}
-else
-{
-    Console.WriteLine("please type CIRCLE or RECTANGLE");
-}
-*/
-/*
-Phone phone=new Phone();
-Android android=new Android();
-ApplePhone applePhone=new ApplePhone();
-
-Console.WriteLine(phone.ShutDown());
-Console.WriteLine(applePhone.ShutDown());
-Console.WriteLine(android.ShutDown());
-*/
-/*
-Boat ford = new Ship();
-Console.WriteLine(ford.BoatMove());
-Console.WriteLine(ford.BoatType());
-*/
-
-/*
-Ivechile vechile =new Ivechile();
-Console.WriteLine(vechile.description());
-Console.WriteLine(vechile.name());
-*/
+        // Area of rectangle and circle according to input by users
+        /*
+        Rectangle rectangle=new Rectangle();
+        Circle circle=new Circle();
+        Console.WriteLine("Type CIRCLE to calculate area of circle, and RECTANGLE to calculate area of rectangle");
+        string shape=Console.ReadLine();
 
 
-//constructor
-/*
-int x = 7;
-Construct con = new Construct();
-Construct con1 = new Construct(x);
-con.Show();
-con1.Showpara();
-con.ShowStatic();
-*/
+        if (shape == "CIRCLE")
+        {
+            circle.Area();
 
-/*
-//calling paramater cons with employee detail
-string a = "hari";
-int b = 20;
-Employee emp = new Employee(a,b);
-Employee copy = new Employee(emp);
-*/
+        }
+        else if (shape=="RECTANGLE")
+        {
+            rectangle.Area();
+        }
+        else
+        {
+            Console.WriteLine("please type CIRCLE or RECTANGLE");
+        }
+        */
+        /*
+        Phone phone=new Phone();
+        Android android=new Android();
+        ApplePhone applePhone=new ApplePhone();
 
+        Console.WriteLine(phone.ShutDown());
+        Console.WriteLine(applePhone.ShutDown());
+        Console.WriteLine(android.ShutDown());
+        */
+        /*
+        Boat ford = new Ship();
+        Console.WriteLine(ford.BoatMove());
+        Console.WriteLine(ford.BoatType());
+        */
 
-/*
-// CallingConvention private constructor
-PriCons.getManager("boi", "258");
-*/
-// int i = 5; i <= 5; i++
-
-int a = 1;
-Console.WriteLine("this is a while loop");
-while (a<=5)
-{
-    Console.WriteLine("the num is "+a);
-    a += 1;
-}
-Console.WriteLine("this is a do while loop");
-int b = 1;
-do
-{
-    Console.WriteLine("the number is :" + b);
-    b += 1;
-}
-while(b<=5);
-
-Console.WriteLine("this is a for loop");
-for(int i = 1; i < 5; i++)
-{
-    Console.WriteLine("the number is :" +i);
-}
-
-Console.WriteLine("this is for each loop");
-//creating lists
-
-List<int>Numbers = new List<int>();
-Numbers.Add(1); 
-Numbers.Add(2);
-Numbers.Add(3);
-Numbers.Add(4);
-Numbers.Add(5);
-
-foreach(int item in Numbers)
-{
-    Console.WriteLine("the number is: "+ item);
-}
+        /*
+        Ivechile vechile =new Ivechile();
+        Console.WriteLine(vechile.description());
+        Console.WriteLine(vechile.name());
+        */
 
 
-Console.ReadLine();
+        //constructor
+        /*
+        int x = 7;
+        Construct con = new Construct();
+        Construct con1 = new Construct(x);
+        con.Show();
+        con1.Showpara();
+        con.ShowStatic();
+        */
+
+        /*
+        //calling paramater cons with employee detail
+        string a = "hari";
+        int b = 20;
+        Employee emp = new Employee(a,b);
+        Employee copy = new Employee(emp);
+        */
+
+
+        /*
+        // CallingConvention private constructor
+        PriCons.getManager("boi", "258");
+        */
+        // int i = 5; i <= 5; i++
+
+        //int a = 1;
+        //Console.WriteLine("this is a while loop");
+        //while (a<=5)
+        //{
+        //    Console.WriteLine("the num is "+a);
+        //    a += 1;
+        //}
+        //Console.WriteLine("this is a do while loop");
+        //int b = 1;
+        //do
+        //{
+        //    Console.WriteLine("the number is :" + b);
+        //    b += 1;
+        //}
+        //while(b<=5);
+
+        //Console.WriteLine("this is a for loop");
+        //for(int i = 1; i < 5; i++)
+        //{
+        //    Console.WriteLine("the number is :" +i);
+        //}
+
+        //Console.WriteLine("this is for each loop");
+        ////creating lists
+
+        //List<int>Numbers = new List<int>();
+        //Numbers.Add(1); 
+        //Numbers.Add(2);
+        //Numbers.Add(3);
+        //Numbers.Add(4);
+        //Numbers.Add(5);
+
+        //foreach(int item in Numbers)
+        //{
+        //    Console.WriteLine("the number is: "+ item);
+        //}
+
+        //for (int i = 1; i < 4; i++)
+        //{
+        //    Console.WriteLine("GeeksforGeeks");
+        //    break;
+        //}
+
+        //for(int i = 1; i <= 10; i++)
+        //{
+        //    if (i == 4)
+        //        continue;
+        //    Console.WriteLine(i);
+        //}
+
+        EvenOdd evenOdd = new EvenOdd();
+        //Console.WriteLine("enter a number");
+        //evenOdd.isEven(Convert.ToInt32(Console.ReadLine()));
+        evenOdd.isEven();
+
+        Console.ReadLine();
+
+        //swich statement
+        /*
+        Console.WriteLine("enter the number of week");
+        string a =Console.ReadLine();
+        int b =Convert.ToInt32(a);
+        if (b < 0)
+        {
+            Console.WriteLine("the number cannot be negative");
+        }
+        else if (b > 7)
+        {
+            Console.WriteLine("the number should not be less than 7");
+        }
+        else
+        {
+            switch (a)
+            {
+                case "1":
+                    Console.WriteLine("its sunday");
+                    break;
+                case "2":
+                    Console.WriteLine("its monday");
+                    break;
+                case "3":
+                    Console.WriteLine("its tuesday");
+                    break;
+                case "4":
+                    Console.WriteLine("its wednusday");
+                    break;
+                case "5":
+                    Console.WriteLine("its thrusday");
+                    break;
+                case "6":
+                    Console.WriteLine("its friday");
+                    break;
+                case "7":
+                    Console.WriteLine("its saturday");
+                    break;
+                default:
+                    Console.WriteLine(a + "should be between 1 and 7");
+                    break;
+            }
+        }
+        
+        
+
+        */
+        Console.ReadLine();
+    
---
+++ first_consol/statement/EvenOdd.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_consol.statement
+{
+    internal class EvenOdd
+    {
+        public void isEven()
+        {
+            //if (a == 0)
+            //{
+            //    Console.WriteLine("invalid digit");
+            //}
+            //else if (a % 2 == 0)
+            //{
+            //    Console.WriteLine("the number " + a + "is a even number");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("the number " + a + "is a odd number");
+            //}
+
+
+
+            //for(int i = 1; i < 10; i++)
+            //{
+            //    if(i == 0)
+            //    {
+            //        Console.WriteLine("invalid number");
+            //    }
+            //    else if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine("the number " + i + " is even");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("the number " + i + " is odd");
+            //    }
+            //}
+
+
+
+            /*
+            //prime or not
+            for (int i = 3; i < 31; i++)
+            {
+                int check = 0;
+                for(int a=2;a<i-1; a++)
+                {
+                    if (i % a == 0)
+                    {
+                        check = 1;
+                        Console.WriteLine(i + "is a not prime number");
+                        break;
+                    }                               
+                }
+                if(check == 0)
+                {
+                    Console.WriteLine(i + "is a  prime number");
+                }
+             }
+            */
+            int n = 123;
+            int rev = 0;
+            if (n > 0)
+            {
+                rev=(rev*10)+(n%10);
+                n = n / 10;
+            }
+            
+            Console.WriteLine(rev);
+            
+
+
+
+        }
+    }
+}
